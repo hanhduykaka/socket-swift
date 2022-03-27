4a2738a57a3ca92d6a369a2c0b0c948453e73eb1
20424113 <20424113@student.hcmus.edu.vn>
2022-03-27 17:25:34 +0700

    update

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ io.on('connection', socket => {
   });
 
   socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', userId)
+    socket.broadcast.emit('user-disconnected', "dis")
   })
 
 })
